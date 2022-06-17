629590bbff69c77b77aab83996c96f9854988ecc
hajduksanchez <hajduksanchez.dev@gmail.com>
2022-06-16 22:20:19 -0500

    Changes

---
+++ src/common/interface/data.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * Data model for data
+ *
+ * @export Interface
+ * @interface DataArray
+ */
+export interface DataArray {
+  data: DataAttributes[];
+}
+export interface DataAttributes {
+  attributes: any;
+}
---
+++ src/components/certificateMiniCard/CertificateMiniCard.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CertificateMiniCard = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export { CertificateMiniCard };
